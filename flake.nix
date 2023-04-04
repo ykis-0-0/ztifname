ffff5c99900a629a0ebdf2fa8d3a976f70426eb8
ykis-0-0 <64165725+ykis-0-0@users.noreply.github.com>
2023-04-04 22:21:31 +0800

    *Bomp*, and NixOS seems to be fucked up on FreeBSD

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
     supportedSystems = with flake-utils.lib.system; [
       aarch64-linux
       x86_64-linux
-      x86_64-freebsd
+      # :-( https://github.com/NixOS/nixpkgs/blob/9572a3b2d849e3ef6962d58f0f8dada076ac1a55/lib/systems/flake-systems.nix#L28
+      # x86_64-freebsd
     ];
     inherit (flake-utils.lib) eachSystem;
   in eachSystem supportedSystems (system: {
